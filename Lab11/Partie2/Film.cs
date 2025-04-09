52aca442e5fd0a0754d2b51adb152f1f7e0f8c46
Oleksandr Bilynets <2431570@etu.cegepjonquiere.ca>
2025-04-09 10:56:28 -0400

    Partie 1- Film

---
+++ Lab11/Partie2/Film.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partie2
+{
+    public partial class Film
+    {
+        string Synopsis { get; set; }
+        List<string> Genres { get; set; }
+
+        public override string ToString()
+        {
+            return $"Le film: {Titre}" ;
+        }
+    }
+}
