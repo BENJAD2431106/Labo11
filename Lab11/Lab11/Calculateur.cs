52453941d4089cc40cbc49a16687627a37c5bbf6
Jad BENTAYAA IDRISSI <bentayaajad@gmail.com>
2025-04-09 09:08:33 -0400

    Partie 1 complétée

---
+++ Lab11/Lab11/Calculateur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class Calculateur
+    {
+        public double Diviser(int entier1, int entier2)
+        {
+            return entier1 / entier2;
+        }
+        public double Multiplier(int entier1, int entier2)
+        {
+            return entier1 * entier2;
+        }
+    }
+}
---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entree1 = 0;
+            int entree2 = 0;
+            Calculateur clc = new Calculateur();
+            Console.WriteLine("Bonjour! Entrez un nombre");
+            try
+            {
+                entree1 = int.Parse(Console.ReadLine());
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Bonjour! Entrez un deuxième nombre");
+            try
+            {
+                entree2 = int.Parse(Console.ReadLine());
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                Console.WriteLine("Multiplication : " + clc.Multiplier(entree1, entree2));
+                Console.WriteLine("Division : " + clc.Diviser(entree1, entree2));
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("On ne peut pas diviser par un nombre nul.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur...");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Try executé avec succès");
+            }
+        }
+    }
+}
