5cdd4a0041ec0cdb7070ed10878e30829e714bd1
Jad BENTAYAA IDRISSI <bentayaajad@gmail.com>
2025-04-09 10:56:01 -0400

    Partie1

---
+++ Lab11/Partie2/Film.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partie2
+{
+    public partial class Film
+    {
+        string Titre {  get; set; }
+        string Directeur { get; set; }
+        int AnneeDeSortie { get; set; }
+        List<String> Acteurs { get; set; }
+
+        public Film(string titre, string directeur, int anneeDeSortie, string synopsis)
+        {
+            Titre = titre;
+            Directeur = directeur;
+            AnneeDeSortie = anneeDeSortie;
+            Acteurs = new List<String>();
+            Genres = new List<String>();
+            Synopsis = synopsis;
+        }
+    }
+}
