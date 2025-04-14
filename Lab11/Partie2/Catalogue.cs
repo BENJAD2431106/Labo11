532db7d3de6dcb4f4fa3fd27062d9441a5c0a0b8
Jad BENTAYAA IDRISSI <bentayaajad@gmail.com>
2025-04-14 09:19:01 -0400

    Partie2-2

---
+++ Lab11/Partie2/Catalogue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partie2
+{
+    public partial class Catalogue
+    {
+        List<Film> films = new List<Film>();
+        public Catalogue()
+        {
+            films = new List<Film>();
+        }
+        public void AddFilm(Film film)
+        {
+            films.Add(film); 
+        }
+        public void DeleteFilms(Film film)
+        {
+            films.Remove(film);
+        }
+        public string GetInfos()
+        {
+            string infos = "";
+            foreach (Film film in films)
+            {
+                infos += film.ToString();
+                infos += "\n";
+            }
+            return infos;
+        }
+        public override string ToString()
+        {
+            return GetInfos();
+        }
+
+
+    }
+}
