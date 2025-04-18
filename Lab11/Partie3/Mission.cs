b42c2c2b97ed901247f921ac7beace1e7603d81f
Oleksandr Bilynets <2431570@etu.cegepjonquiere.ca>
2025-04-18 16:01:20 -0400

    Partie 3 presque terminé

---
+++ Lab11/Partie3/Mission.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partie3
+{
+    public enum Status 
+    {
+        Planifiee,
+        Encours,
+        Terminee
+    }
+    public partial class Mission
+    {
+        string Nom {  get; set; }
+        string Destination { get; set; }
+        DateOnly DateDepart {  get; set; }
+        DateOnly DateArrive { get; set; }
+        Status Status { get; set; }
+
+        public Mission (string nom, string destination, DateOnly dateDepart, DateOnly dateArrive, Status status)
+        {
+            Nom = nom;
+            Destination = destination;
+            DateDepart = dateDepart;
+            DateArrive = dateArrive;
+            Status = status;
+        }
+    }
+}
---
+++ Lab11/Partie3/Vaisseau.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partie3
+{
+    public partial class Vaisseau
+    {
+        public double ChangerVitesse()
+        {
+            try
+            {
+                double nouvelleVitesse = 0;
+                if (nouvelleVitesse >= Vitesse)
+                    throw new ExceptionVitese("La vitesse demandée dépasse la vitesse maximale du vaisseau");
+                else
+                    Vitesse = nouvelleVitesse;
+            }
+            catch (ExceptionVitese exVit)
+            {
+                Console.WriteLine(exVit.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Le changement d'une vitesse a été fait");
+            }
+        }
+    }
+}
